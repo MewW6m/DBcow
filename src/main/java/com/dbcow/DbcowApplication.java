899a9d0164c096574c5814d317d2c9d1b7b39ab4
MewW6m <aheadlink.changat@gmail.com>
2023-05-02 19:25:10 +0900

    first

---
+++ src/main/java/com/dbcow/DbcowApplication.java
@@ -0,0 +1,13 @@
+package com.dbcow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbcowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbcowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbcow/DbcowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbcow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbcowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
