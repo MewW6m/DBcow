7aa3b0d50a21010d309addd642e1a64c27cde94f
MewW6m <aheadlink.changat@gmail.com>
2023-05-05 14:02:01 +0900

    ログイン・新規登録画面実装、thyemeleaf:replaceバグ修正、favicon追加

--- src/main/resources/static/js/body.js
+++ src/main/resources/static/js/body.js
@@ -1,49 +1,49 @@
-const messageType = ['infoMsg', 'successMsg', 'warningMsg', 'errorMsg']
+const messageType = ['#infoMsg', '#successMsg', '#warnMsg', '#errorMsg']
 
 showFirstAlertMsg();
 
 function showAlertMsg(key, message) {
 	if (messageType.includes(key)) {
-		document.querySelector('#' + key + " p").innerText = message;
-		document.querySelector('#' + key).removeAttribute("hidden");
+		document.querySelector(key + " p").innerText = message;
+		document.querySelector(key).removeAttribute("hidden");
 	}
 }
 
 function resetAlertMsg() {
 	for (var key of messageType) {
-		document.querySelector('#' + key + " p").innerText = "";
-		document.querySelector('#' + key).setAttribute("hidden", true);
+		document.querySelector(key + " p").innerText = "";
+		document.querySelector(key).setAttribute("hidden", true);
 	}
 }
 
 function showInfoAlertMsg(message) {
 	resetAlertMsg();
-	showAlertMsg("infoMsg", message);
+	showAlertMsg("#infoMsg", message);
 }
 
 function showSuccessAlertMsg(message) {
 	resetAlertMsg();
-	showAlertMsg("successMsg", message);
+	showAlertMsg("#successMsg", message);
 }
 
-function showWarningAlertMsg(message) {
+function showWarnAlertMsg(message) {
 	resetAlertMsg();
-	showAlertMsg("warningMsg", message);
+	showAlertMsg("#warnMsg", message);
 }
 
 function showErrorAlertMsg(message) {
 	resetAlertMsg();
-	showAlertMsg("errorMsg", message);
+	showAlertMsg("#errorMsg", message);
 }
 	
 function showFirstAlertMsg() {
 	resetAlertMsg();
 
-	const searchParams = new URLSearchParams(window.location.search)
+	const searchParams = new URLSearchParams(window.location.hash)
 	for (var key of searchParams.keys()) {
 		if (messageType.includes(key)) {
-			document.querySelector('#' + key + " p").innerText = searchParams.get(key);
-			document.querySelector('#' + key).removeAttribute("hidden");
+			document.querySelector(key + " p").innerText = searchParams.get(key);
+			document.querySelector(key).removeAttribute("hidden");
 		}
 	}
 }
