35048015d6b5f4536b52e4472e8a92a9b6b4ba3a
MewW6m <aheadlink.changat@gmail.com>
2023-05-09 07:46:26 +0900

    fixエラーハンドリング(より包括的にキャッチするように修正)

--- src/main/java/com/dbcow/config/ErrorHandler.java
+++ src/main/java/com/dbcow/config/ErrorHandler.java
@@ -4,15 +4,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
+import org.hibernate.TypeMismatchException;
+import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.lang.Nullable;
+import org.springframework.validation.BindException;
+import org.springframework.web.ErrorResponseException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.dbcow.model.Response;
@@ -55,5 +71,65 @@ public class ErrorHandler extends ResponseEntityExceptionHandler {
 			errorMessageList.add(error.getField() + ": " + error.getDefaultMessage());
 		});
 		return handleExceptionInternal(ex, errorMessageList.get(0), headers, HttpStatus.INTERNAL_SERVER_ERROR, request);
+	}
+
+	@ExceptionHandler({ Exception.class })
+	public ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) {
+		if (ex instanceof HttpRequestMethodNotSupportedException subEx) {
+			return handleExceptionInternal(subEx, subEx.getMessage(), subEx.getHeaders(), subEx.getStatusCode(),
+					request);
+		} else if (ex instanceof HttpMediaTypeNotSupportedException subEx) {
+			return handleExceptionInternal(subEx, subEx.getMessage(), subEx.getHeaders(), subEx.getStatusCode(),
+					request);
+		} else if (ex instanceof HttpMediaTypeNotAcceptableException subEx) {
+			return handleExceptionInternal(subEx, subEx.getMessage(), subEx.getHeaders(), subEx.getStatusCode(),
+					request);
+		} else if (ex instanceof MissingPathVariableException subEx) {
+			return handleExceptionInternal(subEx, subEx.getMessage(), subEx.getHeaders(), subEx.getStatusCode(),
+					request);
+		} else if (ex instanceof MissingServletRequestParameterException subEx) {
+			return handleExceptionInternal(subEx, subEx.getMessage(), subEx.getHeaders(), subEx.getStatusCode(),
+					request);
+		} else if (ex instanceof MissingServletRequestPartException subEx) {
+			return handleExceptionInternal(subEx, subEx.getMessage(), subEx.getHeaders(), subEx.getStatusCode(),
+					request);
+		} else if (ex instanceof ServletRequestBindingException subEx) {
+			return handleExceptionInternal(subEx, subEx.getMessage(), subEx.getHeaders(), subEx.getStatusCode(),
+					request);
+		} else if (ex instanceof MethodArgumentNotValidException subEx) {
+			List<String> errorMessageList = new ArrayList<>();
+			subEx.getBindingResult().getFieldErrors().forEach(error -> {
+				errorMessageList.add(error.getField() + ": " + error.getDefaultMessage());
+			});
+			return handleExceptionInternal(ex, errorMessageList.get(0), subEx.getHeaders(),
+					HttpStatus.INTERNAL_SERVER_ERROR, request);
+		} else if (ex instanceof NoHandlerFoundException subEx) {
+			return handleExceptionInternal(subEx, subEx.getMessage(), subEx.getHeaders(), subEx.getStatusCode(),
+					request);
+		} else if (ex instanceof AsyncRequestTimeoutException subEx) {
+			return handleExceptionInternal(subEx, subEx.getMessage(), subEx.getHeaders(), subEx.getStatusCode(),
+					request);
+		} else if (ex instanceof ErrorResponseException subEx) {
+			return handleExceptionInternal(subEx, subEx.getMessage(), subEx.getHeaders(), subEx.getStatusCode(),
+					request);
+		} else if (ex instanceof ConversionNotSupportedException theEx) {
+			return handleExceptionInternal(theEx, theEx.getMessage(), new HttpHeaders(),
+					HttpStatus.INTERNAL_SERVER_ERROR, request);
+		} else if (ex instanceof TypeMismatchException theEx) {
+			return handleExceptionInternal(theEx, theEx.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,
+					request);
+		} else if (ex instanceof HttpMessageNotReadableException theEx) {
+			return handleExceptionInternal(theEx, theEx.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,
+					request);
+		} else if (ex instanceof HttpMessageNotWritableException theEx) {
+			return handleExceptionInternal(theEx, theEx.getMessage(), new HttpHeaders(),
+					HttpStatus.INTERNAL_SERVER_ERROR, request);
+		} else if (ex instanceof BindException theEx) {
+			return handleExceptionInternal(theEx, theEx.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST,
+					request);
+		} else {
+			return handleExceptionInternal(ex, ex.getMessage(), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR,
+					request);
+		}
 	}
 }
