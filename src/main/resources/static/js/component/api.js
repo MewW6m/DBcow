590ae0ff842cd3bfae56713eb028605809bb3c96
MewW6m <aheadlink.changat@gmail.com>
2023-05-28 20:45:34 +0900

    jsモジュール化2

--- src/main/resources/static/js/component/api.js
+++ src/main/resources/static/js/component/api.js
@@ -23,6 +23,176 @@ export class Api {
 		return deferred.promise();
 	}
 
+	getTableList(param) {
+		return this.ajax({
+			type: "GET",
+			url: "/api/table/list",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	getTableList2(param) {
+		return this.ajax({
+			type: "GET",
+			url: "/api/table/list2",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	getDataList(param) {
+		return this.ajax({
+			type: "GET",
+			url: "/api/data/list",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	getDataDetail(param) {
+		return this.ajax({
+			type: "GET",
+			url: "/api/data/detail",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	patchDataDetail(param) {
+		return this.ajax({
+			type: "PATCH",
+			url: "/api/data/detail",
+			data: JSON.stringify(param),
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	deleteDataDetail(param) {
+		return this.ajax({
+			type: "DELETE",
+			url: "/api/data/detail",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	getConnectList(param) {
+		return this.ajax({
+			type: "GET",
+			url: "/api/cond/list",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	getConnectDetail(param) {
+		return this.ajax({
+			type: "GET",
+			url: "/api/connect/detail",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	postConnectDetail(param) {
+		return this.ajax({
+			type: "POST",
+			url: "/api/connect/detail",
+			data: JSON.stringify(param),
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	patchConnectDetail(param) {
+		return this.ajax({
+			type: "PATCH",
+			url: "/api/connect/detail",
+			data: JSON.stringify(param),
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	deleteConnectDetail(param) {
+		return this.ajax({
+			type: "DELETE",
+			url: "/api/connect/detail",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	getCondList(param) {
+		return this.ajax({
+			type: "GET",
+			url: "/api/cond/list",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	getCondDetail(param) {
+		return this.ajax({
+			type: "GET",
+			url: "/api/cond/detail",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	postCondDetail(param) {
+		return this.ajax({
+			type: "POST",
+			url: "/api/cond/detail",
+			data: JSON.stringify(param),
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	patchCondDetail(param) {
+		return this.ajax({
+			type: "PATCH",
+			url: "/api/cond/detail",
+			data: JSON.stringify(param),
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	deleteCondDetail(param) {
+		return this.ajax({
+			type: "DELETE",
+			url: "/api/cond/detail",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	postSqlexecute(param) {
+		return this.ajax({
+			type: "POST",
+			url: "/api/sqlexecute",
+			data: JSON.stringify(param),
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
 	/**
 	 * ユーザー取得API
 	 * @param {*} param パラメータ
@@ -92,6 +262,26 @@ export class Api {
 			url: userListPath,
 			data: param,
 			dataType: "json"
+		});
+	}
+
+	getSettingDetail(param) {
+		return this.ajax({
+			type: "GET",
+			url: "/api/setting/detail",
+			data: param,
+			contentType: "application/json",
+			dataType: 'json'
+		});
+	}
+
+	patchSettingDetail(param) {
+		return this.ajax({
+			type: "PATCH",
+			url: "/api/setting/detail",
+			data: JSON.stringify(param),
+			contentType: "application/json",
+			dataType: 'json'
 		});
 	}
 }
