7534e8f35fc0f66cbabb46756d44b09a9fc4d43c
MewW6m <aheadlink.changat@gmail.com>
2023-06-06 07:13:16 +0900

    ユーザー一覧画面実装13

--- src/main/resources/static/js/component/search.js
+++ src/main/resources/static/js/component/search.js
@@ -2,11 +2,7 @@
  **  * 検索条件の描画に関するクラス
  **/
 export class Search {
-    #itemCode = "";
-    #itemName = "";
-    #status = "";
-    #registUser = "";
-    #updateUser = "";
+    #params;
 
     constructor() {
     }
@@ -15,28 +11,13 @@ export class Search {
      **  * 検索パラメータの更新を行う
      **/
     updateSearch() {
-        this.itemCode = $('#itemCodeInput').val();
-        this.itemName = $('#itemNameInput').val();
-        this.status = $('#statusSelect').val();
-        this.registUser = $('#registUserInput').val();
-        this.updateUser = $('#updateUserInput').val();
+        $('.searchItems')
     }
 
     showSearchOneRow() {
         $("#searchForm").addClass("overflow-two-liner");
         $(".searchRow.uk-hidden").first().removeClass("uk-hidden");
     }
-
-    get itemCode() { return this.#itemCode; }
-    set itemCode(arg) { if (typeof arg !== "string") throw new Error(""); this.#itemCode = arg; }
-    get itemName() { return this.#itemName; }
-    set itemName(arg) { if (typeof arg !== "string") throw new Error(""); this.#itemName = arg; }
-    get status() { return this.#status; }
-    set status(arg) { if (typeof arg !== "string") throw new Error(""); this.#status = arg; }
-    get registUser() { return this.#registUser; }
-    set registUser(arg) { if (typeof arg !== "string") throw new Error(""); this.#registUser = arg; }
-    get updateUser() { return this.#updateUser; }
-    set updateUser(arg) { if (typeof arg !== "string") throw new Error(""); this.#updateUser = arg; }
 }
 
 export let search = new Search();
--- src/main/resources/static/js/user/userList.js
+++ src/main/resources/static/js/user/userList.js
@@ -4,36 +4,50 @@ import { list } from "../component/list.js";
 import { search } from "../component/search.js";
 
 class UserList {
-    constructor() {
-        $(window).on('load', function () {
-            let param = {};
-            api.getUserList(param).done((data) => {
-                try {
-                    data.content = userList.repDataContent(data.content);
-                    list.updateLines(data.content);
-                } catch (e) {
-                    console.error(e);
-                    common.showErrorAlertMsg("ユーザー一覧取得が失敗しました。");
-                }
-            }).fail((jqXHR, textStatus, errorThrown) => {
-                common.showErrorAlertMsg("ユーザー一覧取得が失敗しました。\n" + JSON.parse(jqXHR.responseText).content);
-                // ↓ログインしてないとき
-                //location.href = loginPath + "?infoMsg=ログインしてください"
-            });
-        });
+    #param = {};
+    #result = {};
 
-        // ソートボタンを押下したとき
-        $(document).on('click', '#listSection thead th', async function (e) {
-            list.updateArrow(e.currentTarget);
-            await commonSearchLogic();
+/*
+{
+  "search": [
+    {
+      "items": "Jane Doe",
+      "type": "888-555-1212",
+      "Value": "spouse"
+    }
+  ],
+  "sort": {
+    "item": "\"\"",
+    "dir": "\"\""
+  },
+  "page": {
+    "limit": "100",
+    "offset": "0"
+  }
+}
+*/
+
+    constructor() {
+        this.param;
+
+        // 初期表示時
+        $(window).on('load', function () {
+            // this.getUserList(this.param);
         });
 
         // 検索ボタンを押下したとき
         $(document).on("click", '#searchBtn', async function (e) {
             search.updateSearch();
-            await commonSearchLogic();
+            // this.getUserList(this.param);
         });
 
+        // ソートボタンを押下したとき
+        $(document).on('click', '#listSection thead th', async function (e) {
+            list.updateArrow(e.currentTarget);
+            // this.getUserList(this.param);
+        });
+
+        // プラスボタンを押下したとき
         $(document).on("click", '#plusOneRow', async function (e) {
             search.showSearchOneRow();
         });
@@ -42,20 +56,23 @@ class UserList {
     }
 
     // 共通の検索ロジック
-    async commonSearchLogic() {
-        await listApi.fireApi(
-            search.itemCode,
-            search.itemName,
-            search.status,
-            search.registUser,
-            search.updateUser,
-            list.sort,
-            list.order,
-            footer.page
-        );
-        list.updateLines(listApi.result.zaikoList);
+    getUserList(param) {
+        api.getUserList(param).done((data) => {
+            try {
+                data.content = userList.repDataContent(data.content);
+                list.updateLines(data.content);
+            } catch (e) {
+                console.error(e);
+                common.showErrorAlertMsg("ユーザー一覧取得が失敗しました。");
+            }
+        }).fail((jqXHR, textStatus, errorThrown) => {
+            common.showErrorAlertMsg("ユーザー一覧取得が失敗しました。\n" + JSON.parse(jqXHR.responseText).content);
+            // ↓ログインしてないとき
+            //location.href = loginPath + "?infoMsg=ログインしてください"
+        });
     }
 
+    // 一覧表示時置換
     repDataContent(dataContentObj) {
         $.each(dataContentObj, (i1, line) => {
             switch (line["roles"]) {
